b155aed57f1b9c7beb2cda27200104854c6375e5
Ridhiwan <ridhiwanmseya@gmail.com>
2021-09-26 11:18:30 +0300

    Edited the code to include the dataset and the basemap so that it can be publishable

--- tig/app.R
+++ tig/app.R
@@ -1,19 +1,20 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(tidyverse)
 library(leaflet)
 library(leaflet.minicharts)
+load("tzeco2mixx")
+
+tilesURL <- "http://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}"
+basemap <- leaflet(width = "100%", height = "400px") %>%
+  addTiles(tilesURL)
+
+prodRegions <- tzeco2mixx %>% filter(area != "Tanzania")
+prodCols <- names(prodRegions)[6:13]
 
 ui <- fluidPage(
     titlePanel("Tanzania Investment Guide Data"),
-    p("This application shows the current data that is crucial, prior to investing into different lucrativeindustries in Tanzania."),
+    p("This application shows the current data that is crucial, 
+      prior to investing into different lucrative industries in Tanzania."),
     
     sidebarLayout(
         
